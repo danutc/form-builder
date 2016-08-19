ab7c1e912dd6646743bb42042530ae352d080636
link <esurllink@gmail.com>
2016-08-19 14:52:20 +1000

    rename "import" folder to "imports"

--- client/main.jsx
+++ client/main.jsx
@@ -1,143 +1,143 @@
-import App2 from '../import/form_builder/index.jsx';
+import App2 from '../imports/form_builder/index.jsx';
 import React , { Component }  from 'react';
 import { render } from 'react-dom';
-import preset from './preset.js'
+import preset from './preset.js';
 import Form from 'react-jsonschema-form';
-import "../node_modules/react-ui-tree/dist/react-ui-tree.css";
-import customized_widgets from "../import/customized_widgets";
-import "../node_modules/react-datetime/css/react-datetime.css";
+import '../node_modules/react-ui-tree/dist/react-ui-tree.css';
+import customized_widgets from '../imports/customized_widgets';
+import '../node_modules/react-datetime/css/react-datetime.css';
 
 
 const Editor = React.createClass({
-    getInitialState() {
-        return {
-            schema:{},
-            uiSchema:{}
-        }
-    },
+  getInitialState() {
+    return {
+      schema:{},
+      uiSchema:{}
+    };
+  },
 
-    onChange(e){
-        console.log(e.formData);
-        formData = e.formData;
-        this.props.onChange(e.formData);
-    },
+  onChange(e){
+    console.log(e.formData);
+    formData = e.formData;
+    this.props.onChange(e.formData);
+  },
 
-    render(){
-        const schema={
-            "type": "object",
-            "title": "General",
-            "properties": {
-                "name": {
-                    "type": "string",
-                    "title": "Name"
-                },
-                "configs": {
-                    "type": "object",
-                    "title": "JSONSchema",
-                    "properties": {
-                        "defaultValue": {
-                            "type": "string",
-                            "title": "Default Value"
-                        },
-                        "label": {
-                            "type": "string",
-                            "title": "Label"
-                        },
-                        "type": {
-                            "type": "string",
-                            "title": "Input",
-                            "enum": [
-                                "object",
-                                "array",
-                                "integer",
-                                "string",
-                                "boolean"
-                            ]
-                        }
-                    }
-                },
-                "ui": {
-                    "type": "object",
-                    "title": "UISchema",
-                    "properties": {
-                        "classNames": {
-                            "type": "string",
-                            "title": "Class"
-                        },
-                        "placeHolder": {
-                            "type": "string",
-                            "title": "Place Holder"
-                        },
-                        "hint": {
-                            "type": "string",
-                            "title": "Hint"
-                        }
-                    }
-                }
+  render(){
+    const schema={
+      'type': 'object',
+      'title': 'General',
+      'properties': {
+        'name': {
+          'type': 'string',
+          'title': 'Name'
+        },
+        'configs': {
+          'type': 'object',
+          'title': 'JSONSchema',
+          'properties': {
+            'defaultValue': {
+              'type': 'string',
+              'title': 'Default Value'
+            },
+            'label': {
+              'type': 'string',
+              'title': 'Label'
+            },
+            'type': {
+              'type': 'string',
+              'title': 'Input',
+              'enum': [
+                'object',
+                'array',
+                'integer',
+                'string',
+                'boolean'
+              ]
             }
-        };
-        return (<Form
+          }
+        },
+        'ui': {
+          'type': 'object',
+          'title': 'UISchema',
+          'properties': {
+            'classNames': {
+              'type': 'string',
+              'title': 'Class'
+            },
+            'placeHolder': {
+              'type': 'string',
+              'title': 'Place Holder'
+            },
+            'hint': {
+              'type': 'string',
+              'title': 'Hint'
+            }
+          }
+        }
+      }
+    };
+    return (<Form
             schema = {schema}
             uiSchema = {this.state.uiSchema}
             onChange = {this.onChange}
-        />)
-    }
+        />);
+  }
 
 });
 
 const App = React.createClass({
-    getInitialState() {
-        return {
-            schema:{},
-            uiSchema:{}
+  getInitialState() {
+    return {
+      schema:{},
+      uiSchema:{}
+    };
+  },
+
+  onChange(){
+    this.setState({
+      schema:JSON.parse(this.refs.schemaRef.value),
+      uiSchema:JSON.parse(this.refs.uiSchemaRef.value),
+    });
+  },
+
+  onFormData(e){
+
+  },
+
+  render (){
+    const schema={
+      'title': 'A registration form',
+      'description': 'A simple form example.',
+      'type': 'object',
+      'required': [
+        'firstName',
+        'lastName'
+      ],
+      'properties': {
+        'firstName': {
+          'type': 'string',
+          'title': 'First name'
+        },
+        'lastName': {
+          'type': 'string',
+          'title': 'Last name'
+        },
+        'age': {
+          'type': 'integer',
+          'title': 'Age'
+        },
+        'bio': {
+          'type': 'string',
+          'title': 'Bio'
+        },
+        'password': {
+          'type': 'string',
+          'title': 'Password',
+          'minLength': 3
         }
-    },
-
-    onChange(){
-        this.setState({
-            schema:JSON.parse(this.refs.schemaRef.value),
-            uiSchema:JSON.parse(this.refs.uiSchemaRef.value),
-        });
-    },
-
-    onFormData(e){
-
-    },
-
-    render (){
-        const schema={
-            "title": "A registration form",
-            "description": "A simple form example.",
-            "type": "object",
-            "required": [
-                "firstName",
-                "lastName"
-            ],
-            "properties": {
-                "firstName": {
-                    "type": "string",
-                    "title": "First name"
-                },
-                "lastName": {
-                    "type": "string",
-                    "title": "Last name"
-                },
-                "age": {
-                    "type": "integer",
-                    "title": "Age"
-                },
-                "bio": {
-                    "type": "string",
-                    "title": "Bio"
-                },
-                "password": {
-                    "type": "string",
-                    "title": "Password",
-                    "minLength": 3
-                }
-            }
-        };
-        return (<div className="app">
+      }
+    };
+    return (<div className="app">
             <Editor
                 schema={schema}
                 onChange={this.onFormData} />
@@ -163,13 +163,13 @@ const App = React.createClass({
                     onChange={this.updateSchema}>
                 </textarea>
             </div>
-        </div>)
-    },
+        </div>);
+  },
 });
 
 
 
 Meteor.startup(() => {
-    console.log(preset);
-    render(<App2 preset={preset} fields={customized_widgets}/>, document.getElementById('app'));
+  console.log(preset);
+  render(<App2 preset={preset} fields={customized_widgets}/>, document.getElementById('app'));
 });
