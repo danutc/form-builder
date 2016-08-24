0f3039d7aa3378795df46f37a313707f1baaa285
link <esurllink@gmail.com>
2016-08-24 14:32:13 +1000

    add files

---
+++ imports/form_builder/forData2schema.js
@@ -0,0 +1,27 @@
+function formData2schema(formData){
+  if(typeof(formData)=='string'){
+    return {
+      type:'string'
+    };
+  }
+  if(typeof(formData)==number){
+    return {
+      type:'number'
+    };
+  }
+  if(Array.isArray(formData)){
+    return {
+      type: 'array',
+      items: formData2schema(formData[0])
+    };
+  }
+  if(typeof(formData)=='object'){
+    return {
+      type: 'object',
+      properties: Object.keys(formData).reduce((s,a)=>{
+        s[a]=formData2schema(formData[a]);
+        return s;
+      },{})
+    };
+  }
+}
