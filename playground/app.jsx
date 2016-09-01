ae9ba42ae366f95d7eab42fdf18d652c64cb968e
link <esurllink@gmail.com>
2016-09-01 17:01:28 +1000

    update conditional object example

--- playground/app.jsx
+++ playground/app.jsx
@@ -60,8 +60,89 @@ const formSchema = {
                     }
                   }
 */
+
+function myCondition({schema, uiSchema, formData}) {
+
+  if(formData){
+    Object.keys(schema.properties).forEach(function(name){
+      uiSchema[name]=uiSchema[name]||{};
+    });
+    if (formData.switch != 'case1') {
+      uiSchema.case1['ui:widget']='hidden';
+      delete formData.case1;
+    }
+    if (formData.switch != 'case2') {
+      uiSchema.case1['ui:disabled']=true;
+      uiSchema.case2['ui:widget']='hidden';
+      delete formData.case2;
+    }
+    formData.sum = formData.a+formData.b;
+  }
+  return {schema, uiSchema, formData};
+}
+
+function myCondition2({schema, uiSchema, formData}) {
+  if (formData) {
+    function filterProps(props) {
+      let new_properties = {};
+      let new_data = {};
+      props.forEach(function(i) {
+        new_properties[i] = schema.properties[i];
+        new_data = formData[i];
+      });
+      let new_schema = Object.assign({}, schema);
+      new_schema.properties = new_properties;
+      return {
+        schema: new_schema,
+        uiSchema: uiSchema,
+        formData: formData
+      };
+    }
+    let r;
+    if (formData.switch == 'case1') {
+      r = filterProps(['switch', 'case1','a','b','sum']);
+      r.uiSchema.case1 = {
+        'ui:readonly': false
+      };
+    } else if (formData.switch == 'case2') {
+      let r = filterProps(['switch', 'case1', 'case2','a','b','sum']);
+      r.uiSchema.case1 = {
+        'ui:readonly': true
+      };
+    } else {
+      r = filterProps(['switch','a','b','sum']);
+    }
+    r.formData['sum']=r.formData['a']+r.formData['b'];
+    return r;
+  }
+}
 const formSchema = {
-  schema:{"type": "object","conditionFun": "function myCondition({schema, uiSchema, formData}) {\n  if (formData) {\n    function filterProps(props) {\n      let new_properties = {};\n      let new_data = {};\n      props.forEach(function(i) {\n        new_properties[i] = schema.properties[i];\n        new_data = formData[i];\n      });\n      let new_schema = Object.assign({}, schema);\n      new_schema.properties = new_properties;\n      return {\n        schema: new_schema,\n        uiSchema: uiSchema,\n        formData: formData\n      };\n    }\n    if (formData.switch == 'case1') {\n      let r = filterProps(['switch', 'case1']);\n      r.uiSchema.case1 = {\n        'ui:readonly': false\n      };\n      return r;\n    } else if (formData.switch == 'case2') {\n      let r = filterProps(['switch', 'case1', 'case2']);\n      r.uiSchema.case1 = {\n        'ui:readonly': true\n      };\n      return r;\n    } else {\n      return filterProps(['switch']);\n    }\n  }\n}", "properties": { "switch": { "type": "string", "enum": [ "case1", "case2" ] }, "case1": { "type": "string", "default": "case 1" }, "case2": { "type": "string", "default": "case 2" } } },
+  schema:{
+    "type": "object",
+    "conditionFun": myCondition.toString(),
+    "properties": {
+      "switch": {
+        "type": "string",
+        "enum": [ "case1", "case2" ]
+      },
+      "case1": {
+        "type": "string", "default": "case 1"
+      },
+      "case2": {
+        "type": "string", "default": "case 2"
+      },
+      "a":{
+        "type":'number'
+      },
+      "b":{
+        "type":'number'
+      },
+      "sum":{
+        "title":'a+b',
+        "type":'number'
+      }
+    }
+  },
   uiSchema:{  "ui:field": "fullConditional"}
 }
 
